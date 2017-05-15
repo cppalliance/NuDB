03611106eec3562fdd55ccc9ed567df682bfaab9
Robert Ramey <ramey@rrsd.com>
2017-05-15 10:27:31 -0700

    got error_code crammed into Doxygen of error_code.hpp.  Lot's of problems though.  One can see it in the output which is ill formatted, links don't work, and drops information @related and drops headers "description"
    Updated concepts:
    added type traits is_... to hasher and progress.  I don't know that is_Progress makes sense though.  Actually I don't think the Progress concept makes sense.  Progress is a call back from visit and verify.  As such it needs to be callable with particular arguments.  So it can accepted a lambda, function pointer, or function object.  Only a function object makes sense for is_Progress.  So I don't think there is a way of checking is_Progress in the other legal ways of using progress.

--- include/nudb/error.hpp
+++ include/nudb/error.hpp
@@ -8,75 +8,12 @@
 #ifndef NUDB_ERROR_HPP
 #define NUDB_ERROR_HPP
 
-#include <boost/system/system_error.hpp>
 #include <boost/system/error_code.hpp>
 
 namespace nudb {
 
-/// The type of system-specific error code returned by the implementation
-#if GENERATING_DOCS
-class error_code{};
-
-#else
-using boost::system::error_code;
-
-#endif
-
-/// The type of cross-platform error code used by the implementation
-#if GENERATING_DOCS
-class error_condition{};
-
-#else
-using boost::system::error_condition;
-
-#endif
-
-/// The type of system-specific exception used when throwing
-#if GENERATING_DOCS
-class system_error{};
-
-#else
-using boost::system::system_error;
-
-#endif
-
-/// Returns the  category used for system-specific error codes
-#if GENERATING_DOCS
-error_category const&
-system_category();
-
-#else
-using boost::system::system_category;
-
-#endif
-
-/// Returns the category used for cross-platform error codes
-#if GENERATING_DOCS
-error_category const&
-generic_category();
-
-#else
-using boost::system::generic_category;
-
-#endif
-
-/// The base class used for error categories
-#if GENERATING_DOCS
-class error_category{};
-
-#else
-using boost::system::error_category;
-
-#endif
-
-/// The set of constants used for cross-platform error codes
-#if GENERATING_DOCS
-enum errc{};
-
-#else
-namespace errc = boost::system::errc;
-
-#endif
+// lift all error code stuff from boost::system into the nudb namespace
+using namespace boost::system; // system_category, errc
 
 /// Database error codes.
 enum class error
@@ -234,7 +171,7 @@ enum class error
 error_category const&
 nudb_category();
 
-/** Returns a database error code.
+/** Constructs a database error code.
 
     This function is used by the implementation to convert
     @ref error values into @ref error_code objects.
