79c1dcaec8aa54d93979fb56c66ab4d925eda29c
Miguel Portilla <miguelportilla@pobros.com>
2019-05-23 15:11:01 -0400

    Set version to 2.0.1

--- include/nudb/version.hpp
+++ include/nudb/version.hpp
@@ -14,8 +14,8 @@
 //  NUDB_VERSION / 100 % 1000 is the minor version
 //  NUDB_VERSION / 100000 is the major version
 //
-#define NUDB_VERSION 200000
+#define NUDB_VERSION 200001
 
-#define NUDB_VERSION_STRING "2.0.0"
+#define NUDB_VERSION_STRING "2.0.1"
 
 #endif
